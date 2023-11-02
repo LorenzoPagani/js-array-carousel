c3d1158b83c2aabff4de8bd58983e6fe2cf897ca
Lorenzo Pagani <larrypg2@gmail.com>
2023-11-02 15:35:15 +0100

    refactor: adds pics via JS

--- main.js
+++ main.js
@@ -1,5 +1,26 @@
+imagesPath = [
+    "img/01.webp",
+    "img/02.webp",
+    "img/03.webp",
+    "img/04.webp",
+    "img/05.webp"
+];
+
+//Inserisco le immagini nel DOM
+let IncludePics = "";
+for (let i = 0; i < imagesPath.length; i++) {
+    const percorsoImmagine = imagesPath[i];
+    IncludePics += `<img class="picture " src="${percorsoImmagine}">`;
+}
+document.getElementById("carousel").innerHTML = IncludePics;
+
+
+
+
+
+
 let currentImg = 0;
-const images = document.querySelectorAll(".carousel img");
+const images = document.querySelectorAll("#carousel img");
 images[currentImg].classList.add("d-inline-block");
 
 clickNxt = document.getElementById("nextBtn").addEventListener("click", function () {
